4798b3883ee40d8645b9992033c84acc7708f6f2
Bookers <bookers@example.com>
2022-08-30 23:19:27 +0900

    books controller created

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,21 @@
+class BooksController < ApplicationController
+
+  def edit
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+
+  resources :books
+
+  root to: "homes#top"
+  get '/about', to: 'homes#about', as: 'about'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
